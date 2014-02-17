6313ba11c34639e8b3f6ab409e1a9d771c6b0e29
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-02-16 22:39:05 -0800

    url part works, now reading from file/stdin needs to work

--- smiles2zinc.py
+++ smiles2zinc.py
@@ -6,3 +6,21 @@
 #Ryan G. Coleman
 #https://github.com/ryancoleman/zinc-web
 
+import sys
+import urllib2
+import urllib
+
+def smiles2zinc(smilesString):
+  '''function that takes single smiles, returns ZINC code.'''
+  dataDict = {'structure.similarity':'identity', \
+      'structure.smiles':smilesString, \
+      'page.format':'smiles'}
+  smilesEncode = urllib.urlencode(dataDict)
+  request = urllib2.Request(url='http://zinc.docking.org/results/structure', \
+      data=smilesEncode)
+  result = urllib2.urlopen(request)
+  dataOut = result.read()
+  return dataOut
+
+if __name__ == '__main__':
+  print smiles2zinc('COc1ccc(cc1C2OCC(CO2)(CO)[N+](=O)[O-])[N+](=O)[O-]')
