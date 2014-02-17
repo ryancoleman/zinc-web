a4ead5dd6b999592f3712458ece80b8161b15a9f
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-02-17 09:14:58 -0800

    working & parsing

--- smiles2zinc.py
+++ smiles2zinc.py
@@ -6,9 +6,11 @@
 #Ryan G. Coleman
 #https://github.com/ryancoleman/zinc-web
 
-import sys
-import urllib2
-import urllib
+import sys #always, pretty much
+import string #string handling
+import urllib2 #url handling
+import urllib #url encoding
+import argparse
 
 def smiles2zinc(smilesString):
   '''function that takes single smiles, returns ZINC code.'''
@@ -22,5 +24,28 @@ def smiles2zinc(smilesString):
   dataOut = result.read()
   return dataOut
 
+def parse_arguments():
+  '''defines the arguments, returns the parsed arguments (or nothing) if help
+  / usage shown instead'''
+  parser = argparse.ArgumentParser( \
+      description='gets a ZINC code for any smiles')
+  parser.add_argument('infile', nargs='?', default=sys.stdin, \
+      help='infile filenames, default is standard input')
+  arguments = parser.parse_args()
+  return arguments
+
+def run_lots(arguments):
+  '''takes input, reads smiles, passes to the smilez2zinc function'''
+  if arguments.infile == sys.stdin:
+    openfile = arguments.infile
+  else:
+    openfile = open(arguments.infile, 'r')
+  for line in openfile:
+    smilesZinc = smiles2zinc(string.strip(line))
+    print string.strip(smilesZinc)
+  
+
 if __name__ == '__main__':
-  print smiles2zinc('COc1ccc(cc1C2OCC(CO2)(CO)[N+](=O)[O-])[N+](=O)[O-]')
+  #print smiles2zinc('COc1ccc(cc1C2OCC(CO2)(CO)[N+](=O)[O-])[N+](=O)[O-]')
+  arguments = parse_arguments()
+  run_lots(arguments)
