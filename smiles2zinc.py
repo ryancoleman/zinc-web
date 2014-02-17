51f673b29af34af5e65d3360a3c37d44af60fd8b
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-02-16 21:43:52 -0800

    empty script

---
+++ smiles2zinc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+#script reads smiles from a file or command line
+#output smiles with ZINC codes added. 
+#http://zinc.docking.org
+#Ryan G. Coleman
+#https://github.com/ryancoleman/zinc-web
+
