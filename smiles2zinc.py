af91f8c1b8a881bc54b83643b682f88a99de3ea1
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-02-25 07:14:20 -0800

    style fixups

--- smiles2zinc.py
+++ smiles2zinc.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
 #script reads smiles from a file or command line
-#output smiles with ZINC codes added. 
-#http://zinc.docking.org
+#output smiles with ZINC codes added.
+#http://zinc.docking.orgc
 #Ryan G. Coleman
 #https://github.com/ryancoleman/zinc-web
 
-import sys #always, pretty much
-import string #string handling
-import urllib2 #url handling
-import urllib #url encoding
+import sys  # always, pretty much
+import string  # string handling
+import urllib2  # url handling
+import urllib  # url encoding
 import argparse
 
 def smiles2zinc(smilesString):
   '''function that takes single smiles, returns ZINC code.'''
-  dataDict = {'structure.similarity':'identity', \
-      'structure.smiles':smilesString, \
-      'page.format':'smiles'}
+  dataDict = {
+      'structure.similarity': 'identity',
+      'structure.smiles': smilesString,
+      'page.format': 'smiles'}
   smilesEncode = urllib.urlencode(dataDict)
-  request = urllib2.Request(url='http://zinc.docking.org/results/structure', \
-      data=smilesEncode)
+  request = urllib2.Request(
+      url='http://zinc.docking.org/results/structure', data=smilesEncode)
   result = urllib2.urlopen(request)
   dataOut = result.read()
   return dataOut
@@ -27,9 +28,10 @@ def smiles2zinc(smilesString):
 def parse_arguments():
   '''defines the arguments, returns the parsed arguments (or nothing) if help
   / usage shown instead'''
-  parser = argparse.ArgumentParser( \
+  parser = argparse.ArgumentParser(
       description='gets a ZINC code for any smiles')
-  parser.add_argument('infile', nargs='?', default=sys.stdin, \
+  parser.add_argument(
+      'infile', nargs='?', default=sys.stdin,
       help='infile filenames, default is standard input')
   arguments = parser.parse_args()
   return arguments
@@ -44,7 +46,6 @@ def run_lots(arguments):
     smilesZinc = string.strip(smiles2zinc(string.strip(line)))
     for zincline in string.split(smilesZinc, '\n'):
       print string.strip(line), zincline
-  
 
 if __name__ == '__main__':
   #print smiles2zinc('COc1ccc(cc1C2OCC(CO2)(CO)[N+](=O)[O-])[N+](=O)[O-]')
