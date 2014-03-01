9a8a521e2dadfd85dc2cb471af67b19cc623bfba
Ryan G. Coleman <ryancoleman@users.noreply.github.com>
2014-03-01 04:26:43 -0800

    Update smiles2zinc.py

--- smiles2zinc.py
+++ smiles2zinc.py
@@ -2,7 +2,7 @@
 
 #script reads smiles from a file or command line
 #output smiles with ZINC codes added.
-#http://zinc.docking.orgc
+#http://zinc.docking.org
 #Ryan G. Coleman
 #https://github.com/ryancoleman/zinc-web
 
